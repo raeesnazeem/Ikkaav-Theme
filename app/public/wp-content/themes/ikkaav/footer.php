4b68ed347ce434a8ad56d41c6f006ac84ebc61b2
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2024-12-15 14:49:03 +0530

    Initial functions and basic files

---
+++ app/public/wp-content/themes/ikkaav/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Footer Template
+ *
+ * @package Ikkaav
+ *
+ */
+
+?>
+
+<footer>Footer</footer>
+</body>
+</html>
---
+++ app/public/wp-content/themes/ikkaav/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Theme functions
+ *
+ * @package Ikkaav
+ *
+ */
+
