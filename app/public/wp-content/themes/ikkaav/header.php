d5a367cc90d0b6ee961bdaa0c40d98b0c26138d0
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2024-12-18 23:26:08 +0530

    fourth

--- app/public/wp-content/themes/ikkaav/header.php
+++ app/public/wp-content/themes/ikkaav/header.php
@@ -29,7 +29,9 @@ if(function_exists('wp_body_open')){
 ?>
 
 <div id="page" class="site">
-    <header id="masthead" class="site-header" role="banner">Header</header>
+    <header id="masthead" class="site-header" role="banner">
+        <?php get_template_part('template-parts/header/nav');?>
+    </header>
 
     <div id="content" class="site-content">
 
