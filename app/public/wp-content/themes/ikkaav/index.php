86787b24951c2d4cfd8e152f87dcbf8d215ba979
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2024-12-27 23:21:55 +0530

    blog page

--- app/public/wp-content/themes/ikkaav/index.php
+++ app/public/wp-content/themes/ikkaav/index.php
@@ -11,12 +11,19 @@ get_header();
 
 ?>
 
-<div class="content">
-    <?php
-    esc_html_e('Blog Content', 'ikkaav');
-    ?>
+<div id="primary" class="">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php
+            if( have_posts() ){
+                while( have_posts() ) : the_post();
+                the_title();
+                the_excerpt();
+                endwhile;
+            ?>
+        <?php }
+            ?>
+    </main>
 </div>
-
 
 <?php
 get_footer();
