4c5ca4469bb1d947a9442aa720a54dccd5492b86
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-01-01 22:38:20 +0530

    index.php

--- app/public/wp-content/themes/ikkaav/index.php
+++ app/public/wp-content/themes/ikkaav/index.php
@@ -1,64 +1,71 @@
 <?php
-
 /**
- * Main Template file
+ * Main template file.
  *
  * @package Ikkaav
- *
  */
 
 get_header();
 
 ?>
 
-<div id="primary" class="">
-<!--    <main id="main" class="site-main mt-5" role="main">
-
-        <?php
-/*
-        if( is_home() && ! is_front_page() ){
-            */?>
-        <div class="container">
-                <header class="mb-5">
-                <h1 class="page-title">
-                    <?php /*single_post_title(); */?>
-                </h1>
-            </header>
-
+    <div id="primary">
+        <main id="main" class="site-main mt-5" role="main">
             <?php
-/*        }
-
-        */?>
-
-        <?php
-/*            if( have_posts() ){ */?>
-
-                <div class="row">
-
+            if ( have_posts() ) :
+                ?>
+                <div class="container">
                     <?php
-/*                    $index = 0;
-                    $number_of_columns = 3;
-
-                    //start the loop
-                    while( have_posts() ) : the_post();
-                        if( 0 === $index % $number_of_columns ){ */?>
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <?php /*} */?>
-                            <h3><?php /*the_title(); */?></h3>
-                            <div><?php /*the_excerpt() ;*/?></div>
-
-                    <?php
-/*                    $index++;
-
-                    if(0 != $index && 0 === $index % $number_of_columns){
-
+                    if ( is_home() && ! is_front_page() ) {
+                        ?>
+                        <header class="mb-5">
+                            <h1 class="page-title screen-reader-text">
+                                <?php single_post_title(); ?>
+                            </h1>
+                        </header>
+                        <?php
                     }
-                    endwhile; */?>
-                </div> <?php /*}
-            */?>
-        </div>
-    </main>-->
-</div>
+                    ?>
+
+                    <div class="row">
+                        <?php
+                        $index         = 0;
+                        $no_of_columns = 3;
+
+                        while ( have_posts() ) : the_post();
+
+                            if ( 0 === $index % $no_of_columns ) {
+                                ?>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                <?php
+                            }
+
+                            get_template_part( 'template-parts/content' );
+
+                            $index ++;
+
+                            if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+                                ?>
+                                </div>
+                                <?php
+                            }
+
+                        endwhile;
+                        ?>
+                    </div>
+                </div>
+            <?php
+
+            else :
+
+                get_template_part( 'template-parts/content-none' );
+
+            endif;
+
+            ikkaav_pagination();
+            ?>
+        </main>
+    </div>
 
 <?php
 get_footer();
